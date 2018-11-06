ce51635c35d6c0fcaf54c087b730de48432b6f35
Ghada <ghada.muhamed96@gmail.com>
2018-11-06 13:16:16 +0200

    adding logic and AI

---
+++ Move.cpp
@@ -0,0 +1,18 @@
+/*
+ * Move.cpp
+ *
+ *  Created on: Nov 5, 2018
+ *      Author: ghada
+ */
+
+#include "Move.h"
+
+Move::Move() {
+	// TODO Auto-generated constructor stub
+	switchMove = direction = false;
+	x = y = 0;
+	word = "";
+}
+
+
+
